983f3f4cf8dbc9251772d55cb865c1407a3d0440
Juan Camilo <jccaleroe@unal.edu.co>
2016-11-10 11:50:18 -0500

    Optimizacion

--- Matrices/Cuda/matrix.cu
+++ Matrices/Cuda/matrix.cu
@@ -1,47 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <cuda_runtime.h>
+#include <string.h>
 #include <time.h>
+#include <cuda_runtime.h>
 
-int N, *D_N, threads_per_block, blocks_num, threads_num, *d_threads_num;
+#define GPUERRCHK(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+
+inline void gpuAssert(cudaError_t code, char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess)
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
+
+int N, threads_per_block, blocks_num, threads_num;
 float *A, *B, *C, *D_A, *D_B, *D_C, max_num = (float)(RAND_MAX);
 
-#define GPUerrchk(ans) { GPUassert((ans), __FILE__, __LINE__); }
-
-__global__ void multiply( float *A, float *B, float *C, int N, int threads_num ){
+__global__ void multiply( float *A2, float *B2, float *C, int N, int threads_num ){
+	__shared__ float *A;
+	__shared__ float *B;
+	A = A2;	B = B2;
 
  	float tmp;
-	int index = (blockDim.x * blockIdx.x) + threadIdx.x;
- 	int i, j, k, pos, cell_per_thread = N * N / threads_num;
+ 	int k, pos;
 
-	int a = cell_per_thread * index, b = cell_per_thread * (index + 1);
+	int a = N * N * (blockDim.x * blockIdx.x + threadIdx.x) / threads_num, b;
 
-	if (index == threads_num - 1)
+	if ( blockDim.x * blockIdx.x + threadIdx.x == threads_num - 1)
         b = N * N;
+	else
+		b = N * N * ( blockDim.x * blockIdx.x + threadIdx.x + 1) / threads_num;
 
     for( pos = a; pos < b; pos++ ){
-        i = pos / N;
-        j = pos - i*N;
         tmp = 0;
-        for(k = 0; k < N; k++)
-            tmp += A[i*N + k] * B[k*N + j];
-        C[i*N + j] = tmp;
+        for( k = 0; k < N; k++ )
+            tmp += A[ N * (pos / N ) + k ] * B[ k * N + pos - ( pos / N) * N];
+        C[ pos ] = tmp;
     }
 }
 
-int main(int argc, char *agcv){
+int main(int argc, char *argv[]){
 	if(argc != 4){
         printf("Please enter matrix size N\n");
         scanf("%d", &N);
         printf("Please enter number of blocks\n");
         scanf("%d", &blocks_num);
-		printf("Please enter number of threads per block\n");
-        scanf("%d", &threads_num);
+		printf("Please enter number of threads\n");
+        scanf("%d", &threads_per_block);
     }
     else{
         N = atoi(argv[1]);
         blocks_num = atoi(argv[2]);
-		threads_num = atoi(argv[3]);
+		threads_per_block = atoi(argv[3]);
     }
 	
 	int size = N * N * sizeof(float), i, j;
@@ -50,52 +62,45 @@ int main(int argc, char *agcv){
 	B = (float *) malloc ( size );
 	C = (float *) malloc ( size );
 
-//    srand(time(NULL));
-//    for( i = 0; i < N; i++){
-//        for( j = 0; j < N; j++){
-//            A[i*N + j] = max_num / (float) (rand());
-//            B[i*N + j] = max_num / (float) (rand());
-//        }
-//    }
-
-//    test case
+    srand(time(NULL));
     for( i = 0; i < N; i++){
         for( j = 0; j < N; j++){
-            A[i*N + j] = i*N+j;
-            B[i*N + j] = i*N+j;
+            A[i*N + j] = max_num / (float) (rand());
+            B[i*N + j] = max_num / (float) (rand());
         }
     }
 
-	threads_per_block = threads_num / blocks_num;
+//   test case
+//    for( i = 0; i < N; i++){
+//        for( j = 0; j < N; j++){
+//            A[i*N + j] = i*N+j;
+//            B[i*N + j] = i*N+j;
+//        }
+//    }
 
-    GPUerrchk( cudaMalloc ( (void **) &D_A, size ) );
-	GPUerrchk( cudaMemcpy ( D_A, A, size, cudaMemcpyHostToDevice );
+	threads_num	= threads_per_block * blocks_num;
 
-    GPUerrchk( cudaMalloc ( (void **) &D_B, size ) );
-	GPUerrchk( cudaMemcpy ( D_B, B, size, cudaMemcpyHostToDevice );
+    GPUERRCHK( cudaMalloc ( (void **) &D_A, size ) );
+	GPUERRCHK( cudaMemcpy ( D_A, A, size, cudaMemcpyHostToDevice ) );
 
-    GPUerrchk( cudaMalloc ( (void **) &D_C, size ) );
-	GPUerrchk( cudaMemcpy ( D_C, C, size, cudaMemcpyHostToDevice );
+    GPUERRCHK( cudaMalloc ( (void **) &D_B, size ) );
+	GPUERRCHK( cudaMemcpy ( D_B, B, size, cudaMemcpyHostToDevice ) );
 
-    GPUerrchk( cudaMalloc ( (void **) &D_N, sizeof(int) ) );
-	GPUerrchk( cudaMemcpy ( D_N, &N, sizeof(int), cudaMemcpyHostToDevice );
+    GPUERRCHK( cudaMalloc ( (void **) &D_C, size ) );
 
-    GPUerrchk( cudaMalloc ( (void **) &D_threads_num, sizeof(int) ) );
-	GPUerrchk( cudaMemcpy ( D_threads_num, &threads_num, sizeof(int), cudaMemcpyHostToDevice );
-
-    multiply<<<blocks_num, threads_per_block>>>(D_A, D_B, D_C, D_N, D_threads_num);
+    multiply<<<blocks_num, threads_per_block>>>(D_A, D_B, D_C, N, threads_num);
 	
-	GPUerrchk( cudaPeeKAtLastError() );
+	GPUERRCHK( cudaPeekAtLastError() );
 	
-	GPUerrchk( cudaMemcpy(C, D_C, size, cudaMemcpyDeviceToHost) );
+	GPUERRCHK( cudaMemcpy(C, D_C, size, cudaMemcpyDeviceToHost) );
 
-//  print matrix
-    for( i = 0; i < N; i++){
-        for( j = 0; j < N; j++)
-          printf("%0.1f ", C[i*N +j]);
-        printf("\n");
-    }
+//    print matrix
+//    for( i = 0; i < N; i++){
+//        for( j = 0; j < N; j++)
+//          printf("%0.1f ", C[i*N +j]);
+//        printf("\n");
+//    }
 
     free(A); free(B); free(C);
-	GPUerrchk ( cudaDeviceReset() );
+	GPUERRCHK ( cudaDeviceReset() );
 }
